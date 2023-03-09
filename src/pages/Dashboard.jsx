4698e46e0c31a5fae6f3ccdd8bf73e8f557dd370
gopimxelp <gopi.mekala@xelpmoc.in>
2023-03-09 15:24:53 +0530

    dashboard changed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something new</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
